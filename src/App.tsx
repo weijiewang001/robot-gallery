cf170ec6f300be0930ae9b3842f0f86bf6703a76
wayne <weijiewang001@gmail.com>
2023-05-18 16:57:20 +1000

    React 事件处理

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import robots from './mockdata/robots.json'
 import Robot from "./components/Robot"
 import styles from './App.module.css';
+import ShoppingCart from './components/ShoppingCart';
 
 // const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image' />"
 // const jsHacked = "javascript: alert('Hacked!');"
@@ -11,6 +12,11 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt={logo}></img>
+        <h1>罗伯特机器人炫酷online购物平台</h1>
+      </div>
+      <ShoppingCart/>
       <div className={styles.robotList}>
         {/* <div>{html}</div> */}
         {/* <a href={jsHacked}>My websit</a> */}
